bda173fdca8267e8fefa98f7ca17d9c2427b45dd
hoton <i.hoton19@gmail.com>
2022-11-02 22:27:26 +0300

    Initial Commit

---
+++ OurNews/OurNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OurNewsApp.swift
+//  OurNews
+//
+//  Created by hoton on 08/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct OurNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
